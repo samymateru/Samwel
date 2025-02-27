d2f5b6704bb7ab74069ad839faf2d860798e192b
Samuel Alfred <samymateru1999@gmail.com>
2025-02-27 16:51:54 +0300

    refactoring

--- Management/companies/databases.py
+++ Management/companies/databases.py
@@ -11,7 +11,7 @@ from Management.modules import  databases as module_databases
 from collections import defaultdict
 def create_new_company(connection: Connection, company_data: NewCompany) -> int:
     query_insert = """
-        INSERT INTO public.companies (name, owner, email, telephone, website, entity_type, status, created_at) 
+        INSERT INTO public.companies (name, owner, email, telephone, website, entity_type, module_id, status, created_at) 
         VALUES (%s, %s, %s, %s, %s, %s, %s, %s) RETURNING id;
     """
     query_check = "SELECT 1 FROM public.companies WHERE email = %s"
@@ -30,6 +30,7 @@ def create_new_company(connection: Connection, company_data: NewCompany) -> int:
                 company_data.telephone,
                 company_data.website,
                 company_data.entity_type,
+                company_data.module_id,
                 company_data.status,
                 datetime.now()
             ))
