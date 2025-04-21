647531ec4c1abc02862ae717a254847f7909bbaf
Samuel Alfred <samymateru1999@gmail.com>
2025-04-21 12:49:20 +0300

    refactoring

--- AuditNew/Internal/engagements/control/schemas.py
+++ AuditNew/Internal/engagements/control/schemas.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from typing import Optional
 
 class Control(BaseModel):
-    id: Optional[int] = None
+    id: Optional[str] = None
     name: Optional[str]
     objective: Optional[str]
     type: Optional[str]
