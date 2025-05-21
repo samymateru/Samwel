3d24e7b1db6ad7cd2badf51d3566f4c041d445cd
Samuel <samymateru1999@gmail.com>
2025-05-21 11:44:20 +0300

    adding dashboards

--- AuditNew/Internal/dashboards/routes.py
+++ AuditNew/Internal/dashboards/routes.py
@@ -53,13 +53,12 @@ async def fetch_plan_details(
 async def fetch_engagement_details(
         engagement_id: str,
         db=Depends(get_async_db_connection),
-        #user: CurrentUser = Depends(get_current_user)
+        user: CurrentUser = Depends(get_current_user)
 ):
-    #if user.status_code != 200:
-        #raise HTTPException(status_code=user.status_code, detail=user.description)
+    if user.status_code != 200:
+        raise HTTPException(status_code=user.status_code, detail=user.description)
     try:
         data = await query_engagement_details(connection=db, engagement_id=engagement_id)
-
         return data
 
     except HTTPException as e:
